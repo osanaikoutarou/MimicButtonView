6df03911e53659656ea62da5bc5c2124c6e0dc63
osanai <osanai@CR-040.local>
2017-01-25 15:20:31 +0900

    MimicButtonView

---
+++ MimicButtonViewProject/MimicButtonViewProject/MimicButtonView/MimicButtonView.h
@@ -0,0 +1,13 @@
+//
+//  MimicButtonView.h
+//  MimicButtonViewProject
+//
+//  Created by osanai on 2017/01/25.
+//  Copyright © 2017年 osanai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MimicButtonView : UIControl
+
+@end
---
+++ MimicButtonViewProject/MimicButtonViewProject/MimicButtonView/MimicButtonView.m
@@ -0,0 +1,21 @@
+//
+//  MimicButtonView.m
+//  MimicButtonViewProject
+//
+//  Created by osanai on 2017/01/25.
+//  Copyright © 2017年 osanai. All rights reserved.
+//
+
+#import "MimicButtonView.h"
+
+@implementation MimicButtonView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
